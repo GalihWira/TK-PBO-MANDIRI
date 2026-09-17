5a526709e57148ca8ddfa8a5faf928724c8ec397
Galih Wira <galihwira672@gmail.com>
2026-09-17 23:07:21 +0800

    Soal 4

---
+++ Soal 4/src/Musuh.java
@@ -0,0 +1,8 @@
+public class Musuh {
+    String nama;
+    int hp;
+
+    public void muncul(){
+        System.out.println(nama + "telah muncul di arena dengan HP " + hp + "!!");
+    }
+}
---
+++ Soal 4/src/Zombie.java
@@ -0,0 +1,7 @@
+public class Zombie extends Musuh{
+    int kecepatanJalan;
+
+    public void gigit(){
+        System.out.println(nama + " menggigit pemain! (Kecepatan: " + kecepatanJalan + ")");
+    }
+}
