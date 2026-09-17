d976832203371b7cf9b4f1068488b7ec1e1d1ad9
Galih Wira <galihwira672@gmail.com>
2026-09-17 23:52:46 +0800

    Delete accidental Soal 5 folders inside Soal 4

--- Soal 4/src/Musuh.java
+++
@@ -1,8 +0,0 @@
-public class Musuh {
-    String nama;
-    int hp;
-
-    public void muncul(){
-        System.out.println(nama + " telah muncul di arena dengan HP " + hp + "!!");
-    }
-}
--- Soal 4/src/Zombie.java
+++
@@ -1,7 +0,0 @@
-public class Zombie extends Musuh{
-    int kecepatanJalan;
-
-    public void gigit(){
-        System.out.println(nama + " menggigit pemain! (Kecepatan: " + kecepatanJalan + ")");
-    }
-}
