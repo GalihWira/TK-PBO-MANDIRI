1e069699c45db4cae7fce93cd069155bba445188
Galih Wira <galihwira672@gmail.com>
2026-09-18 20:54:51 +0800

    soal 11

---
+++ Soal 11/src/PerbedaanTipeData.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class PerbedaanTipeData {
+    public static void main(String[] args) {
+
+        //1. Contoh Tipe Data Primitif
+        int umur = 20;
+        double ipk = 3.8;
+        float berat = 65.5f;
+        char golonganDarah = 'A';
+        boolean isMahasiswa = true;
+
+        //2. Contoh Tipe Data Primitif
+        String nama = "Galih Wira";
+        double[] nilaiUjian = {80.5, 90.0, 75.5};
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("--- Data Primitif ---");
+        System.out.println(nama + " (Umur: " + umur + ", IPK: " + ipk + ", Berat: " + berat + ", Gol. Darah: " + golonganDarah + ", Mahasiswa: " + isMahasiswa + ")");
+
+        System.out.println("--- Data Referensi ---");
+        System.out.println("Nama (String): " + nama.toLowerCase());
+        System.out.println("Nilai (Array): " + nilaiUjian[0]);
+        System.out.println("Scanner Object: " + input);
+        input.close();
+    }
+}
